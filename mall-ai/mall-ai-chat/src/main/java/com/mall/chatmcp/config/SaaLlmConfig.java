03e00e5e8d92a4fd85d281630ac70ad30d4f34c0
yanff <18170090855@avic-d.com>
2026-06-20 18:50:45 +0800

    init

--- mall-ai/mall-ai-chat/src/main/java/com/mall/chatmcp/config/SaaLlmConfig.java
+++ mall-ai/mall-ai-chat/src/main/java/com/mall/chatmcp/config/SaaLlmConfig.java
@@ -1,17 +1,33 @@
 package com.mall.chatmcp.config;
 
+import com.mall.chatmcp.advisor.RequestAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 @Configuration
 public class SaaLlmConfig {
 
+
     @Bean(name = "qwenChatClient")
     public ChatClient qwenChatClient(OpenAiChatModel model) {
-        return ChatClient.builder(model).build();
+        // 1. 使用 PromptTemplate 加载 md 文件
+        //PromptTemplate systemPromptTemplate = new PromptTemplate(systemPromptResource);
+        // 如果没有变量，直接 render
+        //String systemContent = systemPromptTemplate.render();
+
+        return ChatClient
+            .builder(model)
+            //.defaultSystem(systemContent)
+            .defaultAdvisors(new RequestAdvisor())
+            .defaultAdvisors(new SimpleLoggerAdvisor())
+            .build();
     }
 }
--- mall-ai/mall-ai-chat/src/main/java/com/mall/chatmcp/controller/ChatController.java
+++ mall-ai/mall-ai-chat/src/main/java/com/mall/chatmcp/controller/ChatController.java
@@ -6,12 +6,14 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.SystemMessage;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/ai")
@@ -20,19 +22,16 @@ public class ChatController {
     @Resource(name = "qwenChatClient")
     private ChatClient qwenChatClient;
 
-
-    @Value("${ai.system.prompt}")
-    private String aiSystemPrompt;
-
+    // 注入 系统提示词
+    @Value("classpath:/prompts/system-prompt.md")
+    private org.springframework.core.io.Resource systemPromptResource;
 
     @PostMapping(value = "/chat", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<String> chat(@RequestParam String question) {
+    public Flux<String> chat(@RequestParam String question, @RequestParam String conversationId) {
 
-        SystemMessage systemMessage = new SystemMessage(aiSystemPrompt);
+        SystemMessage systemMessage = new SystemMessage(systemPromptResource);
         UserMessage userMessage = new UserMessage(question);
-
         Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
-
         return qwenChatClient.prompt(prompt)
             .stream()
             .content();
@@ -40,8 +39,7 @@ public class ChatController {
 
     @GetMapping(value = "/send", produces = "text/plain;charset=UTF-8")
     public Flux<String> send(@RequestParam String question) {
-
-        SystemMessage systemMessage = new SystemMessage(aiSystemPrompt);
+        SystemMessage systemMessage = new SystemMessage(systemPromptResource);
         UserMessage userMessage = new UserMessage(question);
 
         Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
